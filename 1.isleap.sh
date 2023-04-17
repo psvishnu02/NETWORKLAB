23c61aa219e6b11f8d9b7925e19f60a54a6c863b
psvishnu02 <116252680+psvishnu02@users.noreply.github.com>
2023-04-17 14:13:35 +0530

    Add files via upload

---
+++ 1.isleap.sh
@@ -0,0 +1,21 @@
+if  [ $# -gt 1 ]
+then
+echo "syntax error"
+exit 1
+fi
+if [ $# -eq 0 ]
+then
+echo "enter the year"
+read year
+else 
+year=$1
+fi
+year4=`expr $year % 4`
+year100=`expr $year % 100`
+year400=`expr $year % 400` 
+if [ $year4 -eq 0 -a $year100 -ne 0 -o $year400 -eq 0 ]
+then
+echo "$year is a leap year"
+else
+echo "$year is not leap"
+fi
---
+++ 2.msgbytime.sh
@@ -0,0 +1,21 @@
+if  [ $# -gt 1 ]
+then
+echo "syntax error"
+exit 1
+fi
+if [ $# -eq 0 ]
+then
+h=$(date +"%H")
+if [ $h -ge 6 -a $h -lt 12 ]
+then
+echo "good morning"
+elif [ $h -ge 12 -a $h -lt 16 ]
+then 
+echo "good afternoon"
+elif [ $h -ge 16 -a $h -lt 20 ]
+then
+echo "good evening"
+else
+echo "good night"
+fi
+fi
