88ecf4df1d5d23d86d6c3248faf7b821cd513f8c
psvishnu02 <116252680+psvishnu02@users.noreply.github.com>
2023-06-15 14:56:31 +0530

    Add files via upload

---
+++ 3.sidoftri.sh
@@ -0,0 +1,26 @@
+if  [ $# -gt 3 -o $# -lt 3 -a $# -gt 0 ]
+then
+echo "syntax error,syntax is $0 [first side] [second side] [third side]"
+exit 1
+fi
+if [ $# -eq 0 ]
+then
+echo "enter first side"
+read a
+echo "enter second side"
+read b
+echo "enter third side"
+read c
+else
+a=$1
+b=$2
+c=$3
+fi
+if [ `expr $a + $b` -le $c -o `expr $a + $c` -le $b -o `expr $b + $c` -le $a ]
+then
+echo "$a $b $c cannot be the sides of triangle"
+exit
+else
+echo "$a $b $c can be the sides of triangle"
+fi
+
---
+++ 4.primeno.sh
@@ -0,0 +1,38 @@
+if [ $# -gt 2 ]
+then
+echo "syntax error"
+exit 1
+fi
+if [ $# -eq 0 ]
+then
+echo "enter begin range" 
+read b
+echo "enter end range" 
+read e
+else
+b=$1
+e=$2
+fi
+if [ $b -gt $e ]
+then
+c=$e
+e=$b
+b=$c
+fi
+echo "prime numbers from $b to $e are:"
+for x in $(seq $b $e)
+do
+k=0
+for i in $(seq 2 $(expr $x - 1))
+do
+        if [ $(expr $x % $i) -eq 0 ]
+   then
+            k=1
+            break
+        fi
+    done
+    if [ $k -eq 0 ]
+    then
+    echo $x
+    fi
+done
