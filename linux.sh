f1df2c03bfea6d45ae01f1598f3619fcbe1ee187
psvishnu02 <116252680+psvishnu02@users.noreply.github.com>
2023-07-14 15:27:45 +0530

    Add files via upload

---
+++ linux.sh
@@ -0,0 +1,28 @@
+#shell script to delete all lines containing word linux*
+if [ $# -gt 1 ]
+then
+echo "Syntax error.\nSyntax is <$0> [file name]"
+exit 1
+fi
+if [ $# -eq 0 ]
+then
+echo "Enter the file name: "
+read file
+else
+file=$1
+fi
+term=`tty`
+exec < $file
+while read line
+do
+echo $line | grep linux > file2
+if [ $? -eq 0 ] 
+then    
+continue
+fi      
+echo $line >> file1
+done
+mv file1 $file
+rm file2
+exec < $term
+exit 0
