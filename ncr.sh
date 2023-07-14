4d21c89af3e329cfe6b58c003d783794497a7aee
psvishnu02 <116252680+psvishnu02@users.noreply.github.com>
2023-07-14 15:07:43 +0530

    Add files via upload

---
+++ ncr.sh
@@ -0,0 +1,36 @@
+#shell script to find nCr using function*
+if [ $# -ne 0 ]
+then 
+echo "Syntax error. Syntax is <$0>"
+exit 1
+fi
+fa(){
+n=$1
+f=1
+while [ $n -gt 0 ]
+do	
+f=`expr $f \* $n`
+n=`expr $n - 1`
+done
+return $f
+}
+ncr(){
+n1=$1
+r1=$2
+fa $n1
+nf=$?
+n_r=`expr $n1 - $r1`
+fa $n_r
+nrf=$?
+fa $r1
+rf=$?
+rnr=`expr $nrf \* $rf`
+nc=`expr $nf / $rnr`
+return $nc
+}
+echo "enter num n: "
+read n2
+echo "enter num r: "
+read r
+ncr $n2 $r
+echo "$n2 C $r=$?"
