f914c8328a490c354e7fe14cd83533cadf7031c0
psvishnu02 <116252680+psvishnu02@users.noreply.github.com>
2023-07-14 14:28:16 +0530

    Add files via upload

---
+++ headtotail.sh
@@ -0,0 +1,35 @@
+#shell script to print specified range of the lines from given files*
+if [ $# -le 0 ]
+then
+echo " Syntax is <$0 > <range> <file....>"
+exit 1
+fi
+n1=$1
+n2=$2
+shift
+shift
+n3=`expr $n2 - $n1 + 1`
+while [ $# -ne 0 ]
+do
+echo " lines from $n1 to $n2 from file $1\n"
+head -$n2 $1 | tail -$n3
+shift
+done
+exit 0
+if [ $# -le 0 ]
+then
+echo " Syntax is <$0 > <range> <file....>"
+exit 1
+fi
+n1=$1
+n2=$2
+shift
+shift
+n3=`expr $n2 - $n1 + 1`
+while [ $# -ne 0 ]
+do
+echo " lines from $n1 to $n2 from file $1\n"
+head -$n2 $1 | tail -$n3
+shift
+done
+exit 0
